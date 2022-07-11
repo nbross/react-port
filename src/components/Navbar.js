1f543e15f4c62d1b16287d53e1950ae1397925e1
nbross <nbross2001@gmail.com>
2022-07-10 20:08:48 -0400

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,31 @@
 import React from "react";
-function Navbar () {
-    
+
+export default function Navbar() {
+    return (
+        <header className="bg-primary md:sticky top-0 z-10">
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <a className="title-font font-medium text-white mb-4 md:mb-0">
+                    <a href="#about" className="ml-3 text-xl text-secondary">
+                        Nbross
+                    </a>
+                </a>
+                <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+                    <a href="#projects" className="mr-5 hover:text-secondary">
+                        Portfolio
+                    </a>
+                    <a href="#skills" className="mr-5 hover:text-secondary">
+                        Skills
+                    </a>
+                    <a href="#testimonials" className="mr-5 hover:text-secondary">
+                        Testimonials
+                    </a>
+                </nav>
+                <a
+                    href="#contact"
+                    className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-secondary rounded text-base mt-4 md:mt-0">
+                    Hire Me
+                </a>
+            </div>
+        </header>
+    );
 }
-export default Navbar;
