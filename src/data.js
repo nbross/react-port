cbc175459602e92aa3860d7a2a73ecf62b8487b7
nbross <nbross2001@gmail.com>
2022-07-10 21:31:34 -0400

    projects

---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+    {
+        title: "Cocktail Buddy",
+        subtitle: "Front-End",
+        description:
+            "A Front-End Based App that's built with html, css, and Javascript. Where you users can find cocktail recipes",
+        image: "https://img.delicious.com.au/F_C2-w6_/w759-h506-cfill/del/2015/11/summer-cocktails-24374-3.jpg",
+        link: "https://nbross.github.io/food-drink-api-project/",
+    },
+    {
+        title: "The Tech Blog",
+        subtitle: "Express and SQL",
+        description:
+            "A blog page made with express that uses a sql db, where users can sign in and create posts.",
+        image: "https://mailrelay.com/wp-content/uploads/2018/03/que-es-un-blog-1.png",
+        link: "https://glacial-fortress-14679.herokuapp.com/",
+    },
+    {
+        title: "Made to Make",
+        subtitle: "Full-Stack",
+        description:
+            "A Full-stack Application where users can login and add items/recipes to a menu.",
+        image: "https://static.onecms.io/wp-content/uploads/sites/9/2020/06/29/creative-pandemic-cooking-essay-FT-BLOG0620.jpg",
+        link: "https://hidden-mesa-12283.herokuapp.com/",
+    },
+    {
+        title: "My Better Portfolio",
+        subtitle: "HTML, CSS, JS",
+        description:
+            "A create and beautiful portfolio showcasing my stuff, personally I think it looks better than this.",
+        image: "./2022-07-10.png",
+        link: "https://nbross.github.io/The_Better_Portfolio/",
+    },
+];
